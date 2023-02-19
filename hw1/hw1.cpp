f020ac3e4efde9dca99e249b39fd81113642a8d3
Anna <annahasagun@yahoo.com>
2023-02-19 09:31:35 +0300

    Initial commit

---
+++ hw1/hw1.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#define MODE 1
+#ifndef MODE
+#error You need to define MODE
+#endif
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+#if MODE == 0
+    std::cout << "Работаю в режиме тренировки \n";
+#elif MODE == 1
+    std::cout << "Работаю в боевом режиме \n";
+    int x, y;
+    std::cout << "Введите число 1: ";
+    std::cin >> x;
+    std::cout << "Введите число 2: ";
+    std::cin >> y;
+    int sum(int x, int y);
+    std::cout << "Результат сложения: " << sum(x, y);
+
+#else 
+    std::cout << "Неизвестный режим. Завершение работы \n";
+#endif
+}
+
+int sum(int x, int y) {
+    return x + y;
+}
+
